b7afdd26cba393fea6da1c373bb277571542be1d
abrahan24 <asaraviacoca@gmail.com>
2024-10-22 11:54:36 -0400

    UPDATE

---
+++ src/test/java/uap/edu/bo/infraestructura/InfraestructuraApplicationTests.java
@@ -0,0 +1,13 @@
+package uap.edu.bo.infraestructura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfraestructuraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
