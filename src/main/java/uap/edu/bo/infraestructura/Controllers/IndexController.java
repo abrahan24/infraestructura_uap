0da8c7478552b1f08b32db657a1a82ec9fe5f888
abrahan24 <asaraviacoca@gmail.com>
2024-10-28 08:32:30 -0400

    UPDATE

--- src/main/java/uap/edu/bo/infraestructura/Controllers/IndexController.java
+++ src/main/java/uap/edu/bo/infraestructura/Controllers/IndexController.java
@@ -1,8 +1,14 @@
 package uap.edu.bo.infraestructura.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
+    
+    @GetMapping("/")
+    public String getMethodName() {
+        return "LOGIN/login";
+    }
     
 }
